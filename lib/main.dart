1daf869210e7a0ae86d9c4e100cf9dbd2459d4eb
LucaBlanc <luca.blanc@outlook.fr>
2021-07-07 18:42:01 +0200

    Main page ended

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           backgroundColor: Colors.transparent,
           appBar: AppBar(
+            shape: Border(bottom: BorderSide(color: Colors.black, width: 1)),
+            elevation: 0,
             titleSpacing: 50,
             title: new Text(
               "MES NOTES",
@@ -21,7 +23,6 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.w700),
             ),
             backgroundColor: Colors.transparent,
-            elevation: 0.0,
           ),
           body: SingleChildScrollView(
             child: Stack(
@@ -81,6 +82,21 @@ class MyCustomFormState extends State<MyCustomForm> {
       key: _formKey,
       child: Column(
         children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "NOUVELLE NOTE",
+                  style: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w100,
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            ),
+          ),
           TextFormField(
             decoration: InputDecoration(
               border: OutlineInputBorder(),
@@ -124,53 +140,61 @@ class MyCustomFormState extends State<MyCustomForm> {
           Spacer(
             flex: 1,
           ),
-          ElevatedButton(
-            style: ButtonStyle(
-                alignment: Alignment.topLeft,
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.black45),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero,
-                        side: BorderSide(color: Colors.black45)))),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              child: Text('Ajouter une image'),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black45),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                          side: BorderSide(color: Colors.black45)))),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                child: Text('Ajouter une image'),
+              ),
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
             ),
-            onPressed: () {
-              // Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
-                // If the form is valid, display a snackbar. In the real world,
-                // you'd often call a server or save the information in a database.
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(SnackBar(content: Text('Processing Data')));
-              }
-            },
           ),
           Spacer(
             flex: 1,
           ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.black45),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero,
-                        side: BorderSide(color: Colors.black45)))),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-              child: Text('AJOUTER MA NOTE'),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black45),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                            side: BorderSide(color: Colors.black45)))),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+                  child: Text('AJOUTER MA NOTE'),
+                ),
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
+                  }
+                },
+              ),
             ),
-            onPressed: () {
-              // Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
-                // If the form is valid, display a snackbar. In the real world,
-                // you'd often call a server or save the information in a database.
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(SnackBar(content: Text('Processing Data')));
-              }
-            },
           ),
         ],
       ),
@@ -201,49 +225,159 @@ class MyNotesState extends State<MyNotes> {
       elevation: 0.0,
       child: Column(
         children: <Widget>[
-          Text(
-            "MES NOTES SAUVEGARDÉES",
-            style: TextStyle(
-                color: Colors.black, fontSize: 30, fontWeight: FontWeight.w700),
-          ),
           Padding(
-            padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-            child: Row(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
+            child: Text(
+              "MES NOTES SAUVEGARDÉES",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              print("oui");
+            },
+            child: Column(
               children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "COURS PHILO #34",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 4, 0, 12),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "07 - 07 - 2021",
+                        style: TextStyle(
+                          color: Colors.black26,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Text(
-                  "COURS PHILO #33",
-                  textAlign: TextAlign.left,
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.",
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w700,
+                    color: Colors.black54,
+                    fontSize: 14,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      ClipRRect(
+                        child: Image.asset(
+                          'assets/images/backflutter.jpg',
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 4, 0, 12),
-            child: Row(
+          Row(
+            children: <Widget>[
+              Container(
+                color: Colors.black87,
+                height: 4,
+                width: 300,
+                margin: EdgeInsets.fromLTRB(0, 40, 0, 20),
+                alignment: Alignment.centerLeft,
+              )
+            ],
+          ),
+          //space
+          InkWell(
+            onTap: () {
+              print("oui2");
+            },
+            child: Column(
               children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "COURS PHILO #33",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 4, 0, 12),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "13 - 06 - 2021",
+                        style: TextStyle(
+                          color: Colors.black26,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Text(
-                  "07 - 07 - 2021",
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.",
                   style: TextStyle(
-                    color: Colors.black26,
-                    fontSize: 13,
+                    color: Colors.black54,
+                    fontSize: 14,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(0),
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 1,
+                            )),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Image.asset(
+                            'assets/images/backflutter.jpg',
+                            height: 150,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-          Text(
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.",
-            style: TextStyle(
-              color: Colors.black54,
-              fontSize: 14,
-            ),
-          ),
-          Image.asset('assets/image/backflutter.jpg'),
         ],
       ),
     );
